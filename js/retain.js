4ea7c8fe32374e2bb8d4f5cea0e149a044d90f3c
thomaskormylo <tkormylo@gmail.com>
2015-09-17 12:27:25 -0600

    Updated script to add timestamps to the note

--- js/retain.js
+++ js/retain.js
@@ -20,13 +20,14 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                dateSubmitted: Date.now()
             });
             view.render();
         },
 
         getNotes: function() {
-            return model.getAllNotes();
+            return model.getAllNotes().reverse();
         },
 
         init: function() {
@@ -51,9 +52,12 @@ $(function(){
         render: function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
+                htmlStr +=  '<li class="note">'+
+                            '<span class="note-date">' +
+                            new Date(note.dateSubmitted).toString() +
+                            '</span>' +
+                            note.content +
+                            '</li>';
             });
             this.noteList.html( htmlStr );
         }
